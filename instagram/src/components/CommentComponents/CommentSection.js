7126ecd4d46f5d3eaa84248534678a56accfa717
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-13 17:48:55 +0800

    Add Comment component

---
+++ instagram/src/components/CommentComponents/CommentSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Comment from './Comment'
+
+const CommentSection = props => {
+  console.log(props)
+  return (
+    <>
+    </>
+  )
+}
+
+export default CommentSection
