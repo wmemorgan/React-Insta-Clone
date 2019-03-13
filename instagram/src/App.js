fa0f80bf0dd50bce43e172e075546cf575b8e58c
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-13 15:51:18 +0800

    Initialize SASS preprocessing

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import dummyData from './dummy-data';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h2>My React App</h2>
       </div>
     );
   }
