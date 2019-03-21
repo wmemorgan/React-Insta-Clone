89b258e310e9653d45577838d640f85e80f70655
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-21 09:27:49 +0800

    Convert to function

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 // High Order Component to manage initial page display
 import withAuthenticate from './components/Authentication/withAuthenticate'
 import Login from './components/Login/Login'
 import PostsPage from './components/PostComponents/PostsPage'
 
-class App extends Component {
-
-  render() {
-    //Instantiate HOC
-    const ComponentFromWithAuthenticate = withAuthenticate(PostsPage)(Login)
-    console.log('App render')
-    return (
-      <div className="App">
-        <ComponentFromWithAuthenticate />
-        {/* <Login /> */}
-      </div>
-    );
-  }
+const App = () => {
+  //Instantiate HOC
+  const ComponentFromWithAuthenticate = withAuthenticate(PostsPage)(Login)
+  console.log('App render')
+  return (
+    <div className="App">
+      <ComponentFromWithAuthenticate />
+      {/* <Login /> */}
+    </div>
+  );
 }
 
 export default App;
