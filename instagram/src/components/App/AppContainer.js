aba62be4cadffdc71ed8e302021e134919c20e27
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-21 12:29:05 +0800

    Remove CSS element reference

--- instagram/src/components/App/AppContainer.js
+++ instagram/src/components/App/AppContainer.js
@@ -4,8 +4,6 @@ const AppContainer = styled.div`
   max-width: 1000px;
   ${props => props.theme.flex('column', 'center')};
   margin: 0 auto;
-  border: 1px solid red;
-  background: dodgerblue;
 `
 
 export default AppContainer
