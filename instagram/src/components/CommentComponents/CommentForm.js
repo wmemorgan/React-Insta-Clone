b1fcd5bc197076729e9ef087272869065ae7eb47
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-21 16:04:57 +0800

    Incorporate Form styled component

--- instagram/src/components/CommentComponents/CommentForm.js
+++ instagram/src/components/CommentComponents/CommentForm.js
@@ -1,12 +1,15 @@
 /* Comment submission form */
 import React from 'react'
 
+// Styled component
+import Form from '../StyleComponents/Form'
+
 const CommentForm = props => {
   const { inputValue, commentInput, addNewComment } = props
   return (
-    <form onSubmit={(e) => addNewComment(e)} className="comment-form">
-      <input onChange={commentInput} value={inputValue} className="comment-input" placeholder="Add comment..." />
-    </form>
+    <Form onSubmit={(e) => addNewComment(e)}>
+      <input onChange={commentInput} value={inputValue} placeholder="Add comment..." />
+    </Form>
   )
 }
 
