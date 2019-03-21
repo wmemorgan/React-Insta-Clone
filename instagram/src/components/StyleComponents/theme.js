65dfe5f7247b7affae8bcca8ae523015042159b1
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-21 15:46:51 +0800

    Create additional color palette option

--- instagram/src/components/StyleComponents/theme.js
+++ instagram/src/components/StyleComponents/theme.js
@@ -8,7 +8,8 @@ const theme = {
     accent: '#ED5D28',
     emphasis: '#03100D',
     darkText: '#595959',
-    lightText: '#FFFFFF'
+    lightText: '#FFFFFF',
+    danger: 'red'
   },
 
   colorScheme: {
